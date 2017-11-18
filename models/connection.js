65fe6322b0f03bcdc3231c78e2ae9c4cfeb1cda1
akusany2 <ankitkumar0411@gmail.com>
2017-11-18 23:29:24 +0530

    Remove comments

--- models/connection.js
+++ models/connection.js
@@ -9,14 +9,8 @@ var db = mongoose.connection;
 
 // db.on('error', console.error.bind(console, 'connection error:'));
 // db.once('open', function() {
-//   var NewCarDb = mongoose.model('NewCar', mongoose.Schema({name: String}));
-//   var newCar = new NewCarDb({
-//     name: 'hola'
-//   });
-//   console.log(newCar.name);
 //   console.log('Connected');
 // });
-
 
 
 module.exports = db;
